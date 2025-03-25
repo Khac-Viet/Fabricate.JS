580dfbc631e1088c9292baa49b68ffaddacaf691
Khac-Viet <viet2ca11@gmail.com>
2025-03-25 13:45:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import crypto from "crypto";
 const path = "./data.json";
 const git = simpleGit();
 
-// Hàm tạo số ngẫu nhiên an toàn bằng crypto
+// Hàm tạo số ngẫu nhiên an toàn bằng crypto layer 1 mạng BTC
 const getRandomInt = (min, max) => Math.floor(crypto.randomInt(min, max + 1));
 
-// Kiểm tra ngày hợp lệ
+// Kiểm tra ngày hợp lệ ( yy/mm/dd/ 2019-now 1-12 1-31 )
 const isValidDate = (date) => {
-    const startDate = moment("2025-01-01");
+    const startDate = moment("2019-01-01");
     const endDate = moment();
     return date.isBetween(startDate, endDate, null, "[]");
 };
@@ -25,13 +25,13 @@ const markCommit = async (date) => {
     await git.commit(date.toISOString(), { "--date": date.toISOString() });
 };
 
-// Tạo commit giả lập
+// Tạo commit giả lập nếu không bị block -> check ngày xong check khối
 const makeCommits = async (n) => {
     for (let i = 0; i < n; i++) {
         const randomWeeks = getRandomInt(0, 58);
         const randomDays = getRandomInt(0, 6);
 
-        const randomDate = moment("2025-01-01")
+        const randomDate = moment("2019-01-01")
             .add(randomWeeks, "weeks")
             .add(randomDays, "days");
 
